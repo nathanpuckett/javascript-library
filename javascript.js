401809d8fed98b005a7981b1e5179af022fab87a
nathanpuckett <nathanfpuckett@gmail.com>
2023-05-04 12:53:32 -0500

    Create addBook... function

--- javascript.js
+++ javascript.js
@@ -1,11 +1,10 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-  // constructor
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
   this.info = function() {
     let readString = ''
     if (read) {
@@ -14,13 +13,24 @@ function Book(title, author, pages, read) {
       readString = 'not read yet'
     }
     return `${title} by ${author}, ${pages} pages, ${readString}`
-  }
-}
+  };
+};
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
+// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 
-console.log(theHobbit.info());
+// console.log(theHobbit.info());
 
 function addBookToLibrary() {
-  
-}
+  const title = prompt('What is the title?');
+  const author = prompt('Who is the author?');
+  const pages = parseInt(prompt('How many pages?'));
+  const read = confirm('Have you read it? ("OK" for yes, "Cancel" for no)');
+
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+};
+
+addBookToLibrary();
+addBookToLibrary();
+
+console.log(myLibrary);
